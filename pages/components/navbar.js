1c80b078dd9144e69d61bf70154d56ff62159ea0
Dev Watts <devwatts5@gmail.com>
2022-05-18 07:46:53 +0000

    -

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -2,26 +2,26 @@ import react from "react";
 import Image from "next/image";
 import Link from 'next/link';
 
-const NavBar = () => {
+const Navbar = () => {
     return (
-        <div className="flex bg-black w-100 justify-between">
+        <div className="flex w-100 justify-between z-50 p-3">
             <div className="flex">
                 <div>
                     <Image src="/static/images/logo.png" width={"80px"} height={"80px"}/>
                 </div>
-                <div className="flex text-white flex-col justify-center	text-center font-bold">
+                <div className="flex text-white flex-col justify-center	text-center font-bold ml-2">
                     <span className="text-2xl	">SOET</span>
                     <span className="text-sm font-bold">SCHOOL OF ENGINEERING AND TECHNOLOGY</span>
                 </div>
             </div>
             <div className="flex flex-row justify-center items-center text-white h-100 text-[17px] font-medium">
-                <Link href="#"><a className="m-3">Announcements</a></Link>
-                <Link href="#"><a className="m-3">Faculty</a></Link>
-                <Link href="#"><a className="m-3">Events</a></Link>
-                <Link href="#"><a className="m-3">Research</a></Link>
+                <Link href="#"><a className="m-3 hover:text-red-400">Announcements</a></Link>
+                <Link href="#"><a className="m-3 hover:text-red-400">Faculty</a></Link>
+                <Link href="#"><a className="m-3 hover:text-red-400">Events</a></Link>
+                <Link href="#"><a className="m-3 hover:text-red-400">Research</a></Link>
             </div>
         </div>
     )
 }
 
-export default NavBar;
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import NavBar from './components/navbar';
+//import styles from '../styles/Home.module.css'
+import Navbar from './components/navbar';
+import '../public/static/css/videocontainer.css'
 
 export default function Home() {
   return (
@@ -11,8 +12,13 @@ export default function Home() {
         <meta name="description" content="SOET Website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NavBar></NavBar>
-      <video playsinline="" src="https://itsmohitchahal.github.io/soet-krmu-static/resources/vid-HD.mp4" type="video/mp4" autoplay="" loop="" muted="" class="front-video"></video>
+      
+      <div className='main-wrapper'>
+          <Navbar></Navbar>
+          <div className='video-container absolute top-0 z-[-10]'>
+              <video autoplay='true' muted loop src='/static/videos/vid-HD.mp4'/>
+          </div>
+      </div>
     </div>
   )
 }
