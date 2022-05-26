1e2b1b23b119bc0a6e3c077306abed3dbaa04cad
Dev Watts <devwatts5@gmaiil.com>
2022-05-26 19:10:56 +0530

    -

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Navbar from './components/navbar';
 import HalfCard from './components/halfCard';
 import FullCard from './components/fullCard';
 import ImageGallery from 'react-image-gallery';
+import Link from 'next/link'
 
 //import "react-image-gallery/styles/scss/image-gallery.scss";
 import "react-image-gallery/styles/css/image-gallery.css";
@@ -49,8 +50,8 @@ export default function Home() {
                   <button className='bg-[#ed3237] text-white p-[15px] transition-all t-[16px] rounded-[100px] hover:tracking-[1px]'>EXPLORE PROGRAMS</button>
               </div>
           </div>
-          <div className='bg-[#ed3237] h-[80px] z-50 w-[100%]'>
-                <span className='ml-[40px] mt-[-110px] md:mt-[-80px] text-white text-[1.5em] font-bold bg-[#006cb5] h-[90px] block p-[20px]	left-[100px] w-fit leading-[50px]'>HIGHLIGHTS</span>
+          <div className='bg-[#ed3237] h-[80px] z-50 w-[100%] mt-[-80px]'>
+                <span className='ml-[40px] text-white text-[1.5em] font-bold bg-[#006cb5] h-[90px] absolute mt-[-10px] p-[20px] w-fit leading-[50px]'>HIGHLIGHTS</span>
           </div>
           <div className='flex md:flex-row flex-col-reverse md:justify-around justify-center mt-[50px] mb-[10px] items-center'>
               <div className='md:w-[40%] w-[90%] flex flex-col'>
@@ -69,7 +70,37 @@ export default function Home() {
               <FullCard width={'90%'}></FullCard>
           </div>
       </div>
-      
+      <div className='w-100 bg-[#3e3e3e] border-t-[8px] border-[#ed3237] h-fit'>
+          <div className='flex flex-col text-white w-100 justify-around mt-[40px] md:flex-row'>
+              <div className='flex w-[90%] self-center flex-col text-[14px] md:w-[45%]'>
+                  <Image src="/static/images/logo.png" height={'100px'} width={'100px'} layout={'fixed'}/>
+                  <Link href={'#'}><a className='mt-[10px] mb-[10px] text-[#91d8f7] font-bold text-[17px]'>Academics</a></Link>
+                  <Link href={'#'}><a className='mt-[10px] mb-[10px] text-[#91d8f7] font-bold text-[17px]'>Admissions</a></Link>
+                  <Link href={'#'}><a className='mt-[10px] mb-[10px] text-[#91d8f7] font-bold text-[17px]'>Faculty</a></Link>
+                  <Link href={'#'}><a className='mt-[10px] mb-[10px] text-[#91d8f7] font-bold text-[17px]'>Examination</a></Link>
+                  <Link href={'#'}><a className='mt-[10px] mb-[10px] text-[#91d8f7] font-bold text-[17px]'>About SOET</a></Link>
+                  <div className='mt-[10px] mb-[20px] flex flex-col w-100'>
+                    <Link href={'#'}><a className='w-[100%] mt-[5px] text-[13px]'>Email: <br></br>welcome@krmangalam.edu.in</a></Link>
+                    <Link href={'#'}><a className='w-[100%] mt-[5px] text-[13px]'>Number: <br></br>01148884888, 8800697010,8800697015</a></Link>
+                    <Link href={'#'}><a className='w-[100%] mt-[5px] text-[13px]'>Location: <br></br>Sohna Road, Gurugram,</a></Link>
+                  </div>
+              </div>
+              <div className='flex w-[90%] self-center justify-center flex-wrap text-[14px] border-t-[8px] border-[#ed3237] md:w-[45%]'>
+                <span className='mt-[30px] mb-[10px] text-[#91d8f7] font-bold text-[17px]'>SUBSCRIBE TO UPDATES</span>
+                    <div className='flex mt-[10px] mb-[30px] bg-[#fff] p-[20px]'>
+                       <form action='/' class="w-full max-w-sm">
+                            <div class="flex items-center border-b border-[#ed3237] py-2">
+                              <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Email Address" aria-label="Full name"/>
+                              <button class="flex-shrink-0 bg-[#ed3237] border-[#ed3237] text-sm border-4 text-white py-1 px-2 rounded" type="submit">
+                                Sign Up
+                              </button>
+                            </div>
+                        </form>
+                    </div>
+              </div>
+              
+          </div>
+      </div>
     </div>
   )
 }
