29cc91b8e6f43e75dd20020a2c94788d50943522
Dev Watts <59016469+devwatts@users.noreply.github.com>
2022-08-26 02:28:49 +0530

    restructured the code

--- utils/constants/routes.constants.js
+++
@@ -1,5 +0,0 @@
-// export const baseUrl = "http://localhost:6001";
-export const baseUrl = "https://krmu-soet-api.herokuapp.com/";
-
-export const facultyLogin = "/auth/faculty/login";
-export const facultyRegister = "/auth/faculty/register";
