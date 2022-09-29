ae2fff8463f3bbbcce2f3d114e03b2c6f04dccce
Dev Watts <59016469+devwatts@users.noreply.github.com>
2022-09-29 13:56:11 +0530

    -

--- components/landing-page/cards/cardRowTwo.js
+++ components/landing-page/cards/cardRowTwo.js
@@ -1,15 +1,22 @@
+import { useState, useEffect } from "react";
+
 const CardRowTwo = (props) => {
-  const splitDescription = props.data
-    ? props.data.event_desc.substring(0, 250)
+  const [cardData, setCardData] = useState({});
+  useEffect(() => {
+    setCardData(props.data);
+  }, []);
+
+  const splitDescription = cardData
+    ? cardData.event_desc?.substring(0, 250)
     : "";
   return (
     <div className="flex flex-col w-[90%] bg-[#ffffff] mt-[30px] p-[15px] items-center rounded-[10px] lg:flex-row md:w-[45%]">
       <div className="mb-[30px] lg:mb-[0px]">
-        <img className="h-[auto] max-w-[200px]" src={props.data.event_cover} />
+        <img className="h-[auto] max-w-[200px]" src={cardData?.event_cover} />
       </div>
       <div className="flex flex-col text-[16px] ml-[20px]">
-        <span className="font-bold text-[19px]">{props.data.event_name}</span>
-        <span className="italic text-[14px]">{props.data.event_date}</span>
+        <span className="font-bold text-[19px]">{cardData?.event_name}</span>
+        <span className="italic text-[14px]">{cardData?.event_date}</span>
         <span className="text-[16px]">
           {splitDescription} <a href="#">more ...</a>
         </span>
