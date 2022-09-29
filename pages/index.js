1a38195869dfe32dc91265b8665a9d1bbba95a21
Dev Watts <59016469+devwatts@users.noreply.github.com>
2022-09-29 12:21:18 +0530

    Achievements fix

--- pages/index.js
+++ pages/index.js
@@ -42,6 +42,7 @@ export default function Home() {
         setAchievementsData(resp);
       });
     }
+    setAchievements();
     setEvents();
     async function setEvents() {
       await API.getAllEvents().then((resp) => {
